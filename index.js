e8b0106b44939230e20b7ec6af66cd68a16bd0e0
Eythan Kramm <eythankramm@Eythans-MacBook-Pro.local>
2019-09-16 19:31:58 -0400

    close galleries with the main menu close

--- index.js
+++ index.js
@@ -16,34 +16,61 @@ menuIcon.addEventListener("click", toggleMenu);
 let gallery = document.querySelector(".img-gallery");
 let galleryButton = document.querySelector(".back-ground-img");
 let closeGallery = document.querySelector(".close-gallery");
+let galleryIsOpen = document.getElementsByClassName(
+  "back-ground-img-gallery-show"
+);
 
 function toggleGallery() {
   gallery.classList.toggle("back-ground-img-gallery-show");
+  console.log(`test equals ${test.length}`);
+}
+
+function closeGalleryWithMenu() {
+  if (galleryIsOpen.length == 1) {
+    gallery.classList.toggle("back-ground-img-gallery-show");
+  }
 }
 
 galleryButton.addEventListener("click", toggleGallery);
 closeGallery.addEventListener("click", toggleGallery);
+closeMenu.addEventListener("click", closeGalleryWithMenu);
 
-// opens and closes the img gallery
+// opens and closes the font gallery
 let fontGallery = document.querySelector(".font-gallery");
 let fontGalleryButton = document.querySelector(".font");
 let closeFontGallery = document.querySelector(".close-fonts");
+let fontGalleryIsOpen = document.getElementsByClassName("font-gallery-show");
 
 function toggleFontGallery() {
   fontGallery.classList.toggle("font-gallery-show");
 }
 
+function closeFontGalleryWithMenu() {
+  if (fontGalleryIsOpen.length == 1) {
+    fontGallery.classList.toggle("font-gallery-show");
+  }
+}
+
 fontGalleryButton.addEventListener("click", toggleFontGallery);
 closeFontGallery.addEventListener("click", toggleFontGallery);
+closeMenu.addEventListener("click", closeFontGalleryWithMenu);
 
 // opens and closes the color picker
 let colorPicker = document.querySelector(".color-gallery");
 let colorPickerButton = document.querySelector(".color-scheme");
 let closeColorPicker = document.querySelector(".close-color");
+let colorGalleryIsOpen = document.getElementsByClassName("color-gallery-show");
 
 function toggleColorPicker() {
   colorPicker.classList.toggle("color-gallery-show");
 }
 
+function closeColorPickerWithMenu() {
+  if (colorGalleryIsOpen.length == 1) {
+    colorPicker.classList.toggle("color-gallery-show");
+  }
+}
+
 colorPickerButton.addEventListener("click", toggleColorPicker);
 closeColorPicker.addEventListener("click", toggleColorPicker);
+closeMenu.addEventListener("click", closeColorPickerWithMenu);
