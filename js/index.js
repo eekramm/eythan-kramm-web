609d80f0d6dc7325431e63b79266c402058fc331
Eythan Kramm <eythankramm@Eythans-MacBook-Pro.local>
2019-10-09 15:37:28 -0400

    update

--- js/index.js
+++ js/index.js
@@ -2,10 +2,16 @@
 let menu = document.querySelector(".menu");
 let menuIcon = document.querySelector(".fa-pencil-square");
 let closeMenu = document.querySelector(".close-menu");
+let aboutMeIcon = document.querySelector(".fa-user-circle-o");
+let portfolioIcon = document.querySelector(".fa-briefcase");
+let contactIcon = document.querySelector(".fa-address-book-o");
 
 function toggleMenu() {
   menu.classList.toggle("menu-show");
   menuIcon.classList.toggle("edit-btn-hide");
+  aboutMeIcon.classList.toggle("about-me-btn-hide");
+  portfolioIcon.classList.toggle("portfolio-btn-hide");
+  contactIcon.classList.toggle("contact-me-btn-hide");
   console.log("Clicked");
 }
 
@@ -74,3 +80,54 @@ function closeColorPickerWithMenu() {
 colorPickerButton.addEventListener("click", toggleColorPicker);
 closeColorPicker.addEventListener("click", toggleColorPicker);
 closeMenu.addEventListener("click", closeColorPickerWithMenu);
+
+// opens and closes the color picker info
+let colorPickerInfo = document.querySelector(".color-gallery-info");
+let colorPickerInfoTriger = document.querySelector("#color-gallery");
+
+function showColorPickerInfo() {
+  colorPickerInfo.classList.toggle("color-gallery-info-show");
+}
+
+colorPickerInfoTriger.addEventListener("mouseenter", showColorPickerInfo);
+colorPickerInfoTriger.addEventListener("mouseleave", showColorPickerInfo);
+
+// opens and closes site-menu
+let bgiButn = document.querySelector(".back-ground-img");
+let fButn = document.querySelector(".font");
+let csButn = document.querySelector(".color-scheme");
+let menuButn = document.querySelector(".site-menu-dropdown");
+let collapseMenu = document.querySelector(".collapse-site-menu");
+let aboutMe = document.querySelector(".about-me");
+let portfolio = document.querySelector(".portfolio");
+let contactMe = document.querySelector(".contact-me");
+
+function toggleSiteMenu() {
+  bgiButn.classList.toggle("back-ground-img-none");
+  fButn.classList.toggle("font-none");
+  csButn.classList.toggle("color-scheme-none");
+  menuButn.classList.toggle("site-menu-dropdown-none");
+  collapseMenu.classList.toggle("collapse-site-menu-show");
+  aboutMe.classList.toggle("about-me-show");
+  portfolio.classList.toggle("portfolio-show");
+  contactMe.classList.toggle("contact-me-show");
+}
+
+menuButn.addEventListener("click", toggleSiteMenu);
+collapseMenu.addEventListener("click", toggleSiteMenu);
+
+// let color = document.getElementById("color");
+// let defaultVal = color.defaultValue;
+// let currentVal = color.value;
+// let trigger = document.querySelector(".color-picker");
+
+// function getCurrentVal() {
+//   let currentVal = color.value;
+//   console.log(currentVal);
+// }
+
+// trigger.addEventListener("mouseover", getCurrentVal);
+
+// console.log(getCurrentVal.value);
+// console.log(defaultVal);
+// console.log(currentVal);
